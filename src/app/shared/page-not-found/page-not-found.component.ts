42847f24569f953e2110261852c35853441be0f9
prafullpatil <prafull.patil@mindbowser.com>
2024-02-06 21:42:19 +0530

    Feat: design page-not-found screen.

--- src/app/shared/page-not-found/page-not-found.component.ts
+++ src/app/shared/page-not-found/page-not-found.component.ts
@@ -1,11 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { SvgIconComponent, provideAngularSvgIcon } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [SvgIconComponent],
+  imports: [SvgIconComponent, RouterModule],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss',
 })
