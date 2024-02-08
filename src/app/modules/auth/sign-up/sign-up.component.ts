72ca137bae32986eca37404a63269003e6b6a337
prafullpatil <prafull.patil@mindbowser.com>
2024-02-08 11:54:26 +0530

    Feat: updated form.

--- src/app/modules/auth/sign-up/sign-up.component.ts
+++ src/app/modules/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegExpConstants } from '../../../core/constants/reg-exp-constants';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +9,44 @@ import { Component } from '@angular/core';
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+  signUpForm!: FormGroup;
+  showPassword: boolean = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) {
+  }
 
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.signUpForm = this.formBuilder.group({
+      email: [
+        null,
+        [
+          Validators.required,
+          Validators.pattern(RegExpConstants.EMAIL_PATTERN),
+        ],
+      ],
+      password: [null, [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  get email(): FormControl {
+    return this.signUpForm.get('email') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.signUpForm.get('password') as FormControl;
+  }
+
+  toggleShow() {
+    this.showPassword = !this.showPassword;
+  }
+
+  signUp(){
+    
+  }
 }
